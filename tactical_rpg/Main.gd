a4d2da2c15ef5b7492f1ddb87d4d23b64233c856
Gakawarstone <pinnekerv@gmail.com>
2022-09-23 09:26:20 +0200

    feat: support multiple players

--- tactical_rpg/Main.gd
+++ tactical_rpg/Main.gd
@@ -3,22 +3,28 @@ extends Node2D
 
 func _ready():
 	var blocks = get_tree().get_nodes_in_group("blocks")
+	var players = get_tree().get_nodes_in_group("players")
+	print(players)
 	for block in blocks:
-		if block.contains_position($Player.position):
-			$Player.block_under = block
-			$Player.block_goto = block
+		for player in players:
+			if block.contains_position($Player.position):
+				player.block_under = block
+				player.block_goto = block
 	
 func _process(delta):
 	var blocks = get_tree().get_nodes_in_group("blocks")
+	var players = get_tree().get_nodes_in_group("players")
 	for block in blocks:
-		if block.contains_position($Player.position):
-			$Player.block_under = block
-		
-		if block.selected && $Player.selected && block != $Player.block_under:
-			$Player.block_under.selected = false
-			$Player.block_goto = block
-			$Player.stage = "Move"
+		for player in players:
+			if block.contains_position(player.position):
+				player.block_under = block
+			
+			if block.selected && player.selected && block != player.block_under:
+				player.block_under.selected = false
+				player.block_goto = block
+				player.stage = "Move"
 	
 	$Debug/Label.text = $Player.stage
+	$Debug/Label2.text = $Player2.stage
 	
 
--- tactical_rpg/Player.gd
+++ tactical_rpg/Player.gd
@@ -7,6 +7,7 @@ var block_goto
 var speed = 5
 var stage = "None"
 
+
 func _ready():
 	pass
 
@@ -20,6 +21,7 @@ func _process(delta):
 		stage = 'Selected'
 
 
+func _physics_process(delta):
 	if stage == 'Move':
 		if (block_goto.get_node("Pivot").get_global_rect().get_center() - position).length() < 5 && block_goto.selected:
 			block_under.selected = false
